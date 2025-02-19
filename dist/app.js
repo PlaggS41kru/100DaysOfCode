f7b1edaf8f4cb7d370446f66ee93009761fb3e59
PlaggS41kru <neris.gilson@hotmail.com>
2025-02-19 10:48:48 -0300

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//13 - Criar uma função que verifica se um número está dentro de um intervalo específico.
+let array1 = [10, 12, 3];
+const veri = array1.some((item) => item === 10);
+console.log(veri);
